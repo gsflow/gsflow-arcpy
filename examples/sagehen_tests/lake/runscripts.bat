8d15ec077d53b15e25b1897ffce8f3ce953dc7d6
Charles Morton <cgmorton@users.noreply.github.com>
2018-10-09 14:53:06 -0700

    Updated lake and terminal basin test examples
    Rebuilt lake and terminal elevation rasters as 32bit floats instead of 64bit floats.
    Added runscript batch files.
    Added "impervious" folder to the gitignore.

--- examples/sagehen_tests/lake/runscripts.bat
+++ examples/sagehen_tests/lake/runscripts.bat
@@ -1,16 +1,16 @@
-python ..\..\..\scripts\fishnet_generator.py -i test_parameters_lakes.ini --overwrite
+python ..\..\..\scripts\fishnet_generator.py -i test_parameters_lake.ini --overwrite
 
-python ..\..\..\scripts\hru_parameters.py -i test_parameters_lakes.ini
-python ..\..\..\scripts\dem_parameters.py -i test_parameters_lakes.ini
-python ..\..\..\scripts\dem_2_streams.py -i test_parameters_lakes.ini
-python ..\..\..\scripts\crt_fill_parameters.py -i test_parameters_lakes.ini
-python ..\..\..\scripts\dem_2_streams.py -i test_parameters_lakes.ini
-python ..\..\..\scripts\crt_fill_parameters.py -i test_parameters_lakes.ini
-python ..\..\..\scripts\stream_parameters.py -i test_parameters_lakes.ini
-python ..\..\..\scripts\veg_parameters.py -i test_parameters_lakes.ini
-python ..\..\..\scripts\soil_raster_prep.py -i test_parameters_lakes.ini
-python ..\..\..\scripts\soil_parameters.py -i test_parameters_lakes.ini
-python ..\..\..\scripts\prism_800m_normals.py -i test_parameters_lakes.ini
-python ..\..\..\scripts\ppt_ratio_parameters.py -i test_parameters_lakes.ini
-python ..\..\..\scripts\impervious_parameters.py -i test_parameters_lakes.ini
-python ..\..\..\scripts\prms_template_fill.py -i test_parameters_lakes.ini
+python ..\..\..\scripts\hru_parameters.py -i test_parameters_lake.ini
+python ..\..\..\scripts\dem_parameters.py -i test_parameters_lake.ini
+python ..\..\..\scripts\dem_2_streams.py -i test_parameters_lake.ini
+python ..\..\..\scripts\crt_fill_parameters.py -i test_parameters_lake.ini
+python ..\..\..\scripts\dem_2_streams.py -i test_parameters_lake.ini
+python ..\..\..\scripts\crt_fill_parameters.py -i test_parameters_lake.ini
+python ..\..\..\scripts\stream_parameters.py -i test_parameters_lake.ini
+python ..\..\..\scripts\veg_parameters.py -i test_parameters_lake.ini
+python ..\..\..\scripts\soil_raster_prep.py -i test_parameters_lake.ini
+python ..\..\..\scripts\soil_parameters.py -i test_parameters_lake.ini
+python ..\..\..\scripts\prism_800m_normals.py -i test_parameters_lake.ini
+python ..\..\..\scripts\ppt_ratio_parameters.py -i test_parameters_lake.ini
+python ..\..\..\scripts\impervious_parameters.py -i test_parameters_lake.ini
+python ..\..\..\scripts\prms_template_fill.py -i test_parameters_lake.ini
