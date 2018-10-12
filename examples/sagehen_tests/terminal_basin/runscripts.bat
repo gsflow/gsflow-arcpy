3d7f81bb6989decc3cf12edc233abc7ec788b8d7
murphyg <murphygardner@gmail.com>
2018-10-12 16:20:32 -0700

    terminal basin test gsflow run

--- examples/sagehen_tests/terminal_basin/runscripts.bat
+++ examples/sagehen_tests/terminal_basin/runscripts.bat
@@ -1,16 +1,16 @@
-python ..\..\..\scripts\fishnet_generator.py -i terminal_basin_parameters.ini --overwrite
+..\scripts\fishnet_generator.py -i terminal_basin_parameters.ini --overwrite
 
-python ..\..\..\scripts\hru_parameters.py -i terminal_basin_parameters.ini
-python ..\..\..\scripts\dem_parameters.py -i terminal_basin_parameters.ini
-python ..\..\..\scripts\dem_2_streams.py -i terminal_basin_parameters.ini
-python ..\..\..\scripts\crt_fill_parameters.py -i terminal_basin_parameters.ini
-python ..\..\..\scripts\dem_2_streams.py -i terminal_basin_parameters.ini
-python ..\..\..\scripts\crt_fill_parameters.py -i terminal_basin_parameters.ini
-python ..\..\..\scripts\stream_parameters.py -i terminal_basin_parameters.ini
-python ..\..\..\scripts\veg_parameters.py -i terminal_basin_parameters.ini
-python ..\..\..\scripts\soil_raster_prep.py -i terminal_basin_parameters.ini
-python ..\..\..\scripts\soil_parameters.py -i terminal_basin_parameters.ini
-python ..\..\..\scripts\prism_800m_normals.py -i terminal_basin_parameters.ini
-python ..\..\..\scripts\ppt_ratio_parameters.py -i terminal_basin_parameters.ini
-python ..\..\..\scripts\impervious_parameters.py -i terminal_basin_parameters.ini
-python ..\..\..\scripts\prms_template_fill.py -i terminal_basin_parameters.ini
+..\scripts\hru_parameters.py -i terminal_basin_parameters.ini
+..\scripts\dem_parameters.py -i terminal_basin_parameters.ini
+..\scripts\dem_2_streams.py -i terminal_basin_parameters.ini
+..\scripts\crt_fill_parameters.py -i terminal_basin_parameters.ini
+..\scripts\dem_2_streams.py -i terminal_basin_parameters.ini
+..\scripts\crt_fill_parameters.py -i terminal_basin_parameters.ini
+..\scripts\stream_parameters.py -i terminal_basin_parameters.ini
+..\scripts\veg_parameters.py -i terminal_basin_parameters.ini
+..\scripts\soil_raster_prep.py -i terminal_basin_parameters.ini
+..\scripts\soil_parameters.py -i terminal_basin_parameters.ini
+..\scripts\prism_800m_normals.py -i terminal_basin_parameters.ini
+..\scripts\ppt_ratio_parameters.py -i terminal_basin_parameters.ini
+..\scripts\impervious_parameters.py -i terminal_basin_parameters.ini
+..\scripts\prms_template_fill.py -i terminal_basin_parameters.ini
